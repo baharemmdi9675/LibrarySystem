a8014e1208608bda3197f93a12c1228757f491c9
bahar <baharmohammadi6346@gmail.com>
2026-09-17 16:09:18 +0330

    Implement Random Generate

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
 
+    Random rand = new Random();
+    int randNum = rand.nextInt();
+    System.out.println(randNum);
 }
